1a0f28511b12b844eb98811925b5ffae8c4667d3
Scott Peshak <scott.peshak@meredith.com>
2020-12-31 11:33:19 -0600

    Install coc libraies during vim setup

--- run_once_vim.sh
+++ run_once_vim.sh
@@ -4,3 +4,6 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Force a Vundle install of all plugins
 vim -c ":PluginInstall" -c ":qa"
+
+# Install the CoC libaries
+vim -c ":call coc#util#install()"
