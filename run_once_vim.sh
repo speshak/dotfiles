d8a8ed5bd37eb9ff45b9a483959f13f359e623f1
Scott Peshak <scott@peshak.net>
2023-02-28 13:36:50 -0600

    Remove old vim specific install steps

--- run_once_vim.sh
+++ run_once_vim.sh
@@ -2,12 +2,3 @@
 
 # Install packer & plugins
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-
-
-# Install the CoC libaries
-vim -c ":call coc#util#install()"
-
-vim -c ":CocInstall coc-json" -c ":qa"
-vim -c ":CocInstall coc-pyright" -c ":qa"
-vim -c ":CocInstall coc-sh" -c ":qa"
-vim -c ":CocInstall coc-clangd" -c ":qa"
