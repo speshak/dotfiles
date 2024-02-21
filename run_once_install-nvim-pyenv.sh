319650e7f7a4413727cb11a52d5dde6105dff464
Scott Peshak <scott_peshak@mckinsey.com>
2024-02-21 09:17:34 -0600

    Improve nvim-pyenv script

--- run_once_install-nvim-pyenv.sh
+++ run_once_install-nvim-pyenv.sh
@@ -1,11 +1,19 @@
-#!/bin/bash
+#!/bin/bash -e
 VER=3.10.5
 VENV_NAME=py3nvim
 
 # Load pyenv
+eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init - bash)"
 
 pyenv install --skip-existing ${VER}
-pyenv virtualenv ${VER} ${VENV_NAME}
+
+# Create the venv if it doesn't exist
+if ! pyenv virtualenvs | grep -q ${VENV_NAME}
+then
+  pyenv virtualenv ${VER} ${VENV_NAME}
+fi
+
+# Activate the venv and install the required pip modules
 pyenv activate ${VENV_NAME}
 python3 -m pip install pynvim
